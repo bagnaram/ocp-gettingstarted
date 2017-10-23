5ed7aa50c6f093e05ea06c67c3c07e063eca1174
Matt Bagnara <mbagnara@redhat.com>
2017-10-23 10:19:48 -0400

    composer

--- index.php
+++ index.php
@@ -8,10 +8,39 @@ $config = ['settings' => [
 ]];
 $app = new App($config);
 
-// Define app routes
-$app->get('/hello/{name}', function ($request, $response, $args) {
-    return $response->write("Hello " . $args['name']);
-});
+$app->get('/', function ($request, $response, $args) {
+  return $this->view->render($response, 'home.html', [
+      'current' => 'home'
+  ]);
+})->setName('current');
+
+
+$app->get('/ldap/{query}', function ($request, $response, $args) {
+
+  // $ds is a valid link identifier for a directory server
+
+  // $person is all or part of a person's name, eg "Jo"
+
+  $ds = "ldap://ldap01.intranet.prod.int.rdu2.redhat.com/"
+  $dn = "dc=redhat,dc=com";
+
+  $query = $args['query']
+  $filter="(|(sn=$query*)(givenname=$query*))";
+  $justthese = array("ou", "sn", "givenname", "mail");
+
+  $sr=ldap_search($ds, $dn, $filter, $justthese);
+
+  $info = ldap_get_entries($ds, $sr);
+
+#  echo $info["count"]." entries returned\n";
+
+
+  return $this->view->render($response, 'ldapsearch.html', [
+      'query' => $query,
+      'resultset' => $info
+  ]);
+})->setName('ldap');
+
 
 // Run app
 $app->run();
