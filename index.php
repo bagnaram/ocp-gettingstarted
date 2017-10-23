878fe0795c5c29f7fab2723f16da82e2f5bbd811
Matt Bagnara <mbagnara@redhat.com>
2017-10-23 09:58:19 -0400

    composer

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 require 'vendor/autoload.php';
+use \Slim\App;
 
 // Create and configure Slim app
 $config = ['settings' => [
     'addContentLengthHeader' => false,
 ]];
-$app = new \Slim\App($config);
+$app = new App($config);
 
 // Define app routes
 $app->get('/hello/{name}', function ($request, $response, $args) {
