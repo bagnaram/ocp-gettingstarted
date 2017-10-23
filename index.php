38248c567d40aefa15aa6fa1c2e139e92bb394b0
Matt Bagnara <mbagnara@redhat.com>
2017-10-23 11:00:24 -0400

    composer

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ $config = ['settings' => [
 ]];
 $app = new \Slim\App($config);
 
+$container['view'] = new \Slim\Views\PhpRenderer("../templates/");
+
 $app->get('/home', function ($request, $response, $args) {
   return $this->view->render($response, "home.html");
 })->setName('homepage');
